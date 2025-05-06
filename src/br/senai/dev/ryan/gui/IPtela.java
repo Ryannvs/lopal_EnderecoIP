7082285d090eb8a7021d052c8992471d9a6fc17a
Ryannvs <ryannvs89@gmail.com>
2025-05-06 09:05:07 -0300

    Lógica reformulada para orientação a objeto

--- src/br/senai/dev/ryan/gui/IPtela.java
+++ src/br/senai/dev/ryan/gui/IPtela.java
@@ -6,7 +6,10 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+import br.senai.dev.ryan.model.EnderecoIP;
 
 public class IPtela {
 	private JLabel lblIP;
@@ -34,66 +37,77 @@ public class IPtela {
 		lblIP.setText("Endereço IP");
 		lblIP.setBounds(223, 10, 200, 50);
 		tela.getContentPane().add(lblIP);
-		
+
 		JTextField txtOctate1 = new JTextField();
 		txtOctate1.setBounds(20, 70, 80, 40);
 		txtOctate1.setHorizontalAlignment(JTextField.CENTER);
 		tela.getContentPane().add(txtOctate1);
-		
+
 		JLabel lblPonto1 = new JLabel();
 		lblPonto1.setText(".");
 		lblPonto1.setBounds(107, 70, 80, 40);
 		tela.getContentPane().add(lblPonto1);
-		
+
 		JTextField txtOctate2 = new JTextField();
 		txtOctate2.setBounds(120, 70, 80, 40);
 		txtOctate2.setHorizontalAlignment(JTextField.CENTER);
 		tela.getContentPane().add(txtOctate2);
-		
+
 		JLabel lblPonto2 = new JLabel();
 		lblPonto2.setText(".");
 		lblPonto2.setBounds(207, 70, 80, 40);
 		tela.getContentPane().add(lblPonto2);
-		
+
 		JTextField txtOctate3 = new JTextField();
 		txtOctate3.setBounds(220, 70, 80, 40);
 		txtOctate3.setHorizontalAlignment(JTextField.CENTER);
 		tela.getContentPane().add(txtOctate3);
-		
+
 		JLabel lblPonto3 = new JLabel();
 		lblPonto3.setText(".");
 		lblPonto3.setBounds(307, 70, 80, 40);
 		tela.getContentPane().add(lblPonto3);
-		
+
 		JTextField txtOctate4 = new JTextField();
 		txtOctate4.setBounds(320, 70, 80, 40);
 		txtOctate4.setHorizontalAlignment(JTextField.CENTER);
 		tela.getContentPane().add(txtOctate4);
-		
+
 		JLabel lblBarra = new JLabel();
 		lblBarra.setText("/");
 		lblBarra.setBounds(407, 70, 80, 40);
 		tela.getContentPane().add(lblBarra);
-		
+
 		JTextField txtOctate5 = new JTextField();
 		txtOctate5.setBounds(420, 70, 80, 40);
 		txtOctate5.setHorizontalAlignment(JTextField.CENTER);
 		tela.getContentPane().add(txtOctate5);
-		
+
 		JButton btnMostrarDetalhes = new JButton();
 		btnMostrarDetalhes.setText("Mostrar Detalhes do IP");
 		btnMostrarDetalhes.setBounds(160, 150, 200, 40);
 		tela.getContentPane().add(btnMostrarDetalhes);
-		
+
 		btnMostrarDetalhes.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				
+				try {  //Monta o IP digitado 
+					String ipCompleto = txtOctate1.getText() + "." +
+						 txtOctate2.getText() + "." + txtOctate3.getText() + "." +
+						 txtOctate4.getText() + "/" + txtOctate5.getText();
+					 EnderecoIP endereco = new EnderecoIP(ipCompleto);
+					  String detalhes = "Classe: " + endereco.getClasse() + "\n"
+					 + "Máscara Decimal: " + endereco.getMascaraDecimal() + "\n" + "Máscara Binária: " + endereco.getMascaraBinaria() + "\n" + "Total de IPs: " +
+					 endereco.getTotalIPs();
+					// Mostra os dados em uma caixa de diálogo
+					 JOptionPane.showMessageDialog(null,
+					 detalhes, "Detalhes do IP", JOptionPane.INFORMATION_MESSAGE);
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(null, "Erro: entrada inválida.", "Erro", JOptionPane.ERROR_MESSAGE);
+				}
 			}
 		});
-
 
 		tela.setVisible(true);
 	}
---
+++ src/br/senai/dev/ryan/model/EnderecoIP.java
@@ -0,0 +1,43 @@
+package br.senai.dev.ryan.model;
+
+public class EnderecoIP {
+	private String ip;
+	private int cidr;
+
+	public EnderecoIP(String entrada) {
+		String[] partes = entrada.split("/");
+		ip = partes[0];
+		cidr = Integer.parseInt(partes[1]);
+	}
+
+	public String getClasse() {
+		int primeiro = Integer.parseInt(ip.split("\\.")[0]);
+		if (primeiro <= 127)
+			return "Classe A";
+		if (primeiro <= 191)
+			return "Classe B";
+		if (primeiro <= 223)
+			return "Classe C";
+		if (primeiro <= 239)
+			return "Classe D";
+
+		return "Classe E";
+	}
+
+	public String getMascaraDecimal() {
+		int mascara = 0xFFFFFFFF << (32 - cidr);
+		return (mascara >>> 24 & 0xFF) + "." + (mascara >>> 16 & 0xFF) + "." + (mascara >>> 8 & 0xFF) + "."
+				+ (mascara & 0xFF);
+	}
+
+	public String getMascaraBinaria() {
+		int mascara = 0xFFFFFFFF << (30 - cidr);
+		return String.format("%8s.%8s.%8s.%8s", Integer.toBinaryString(mascara >>> 24 & 0xFF),
+				Integer.toBinaryString(mascara >>> 16 & 0xFF), Integer.toBinaryString(mascara >>> 8 & 0xFF),
+				Integer.toBinaryString(mascara & 0xFF)).replace(' ', '0');
+	}
+
+	public int getTotalIPs() {
+		return (int) Math.pow(2, 30 - cidr);
+	}
+}
